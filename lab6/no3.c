7f2a06759919e65c72df0523773818b18bde9c20
SueaWS <witsarut1133@outlook.com>
2023-01-15 18:58:21 +0700

    Add .gitignore again

--- lab6/no3.c
+++ lab6/no3.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
-int main()
-{
+int main(){
 	int total=0,arr[13],check,check2;
-	char s1[14];
+	char string[14];
     printf("Please Enter ID: ");
-	scanf("%s",s1);
+	scanf("%s",string);
 	for(int i=0,j=13;i<12;i++,j--){
-		arr[i] = (s1[i] - '0')*j;
+		arr[i] = (string[i] - '0')*j;
 		total = total+arr[i];
 	}
 	check = total%11;
@@ -16,7 +14,7 @@ int main()
 	if(check2 >= 10){
 		check2 = check2%10;
 	}
-	if(check2 == s1[12]-'0')
+	if(check2 == string[12]-'0')
 		printf("Your ID is Valid");
 	else
 		printf("Your ID is Invalid");
