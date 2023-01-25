ad50f8c9ebd6a8492100c2ba7de35f0eeeef7886
SueaWS <witsarut1133@outlook.com>
2023-01-25 16:28:58 +0700

    done

--- Grader/aws11.c
+++ Grader/aws11.c
@@ -1,38 +1,119 @@
 #include<stdio.h>
-
+#include<math.h>
 int main(){
-    int m,n;
-    int tempsum=0,endsum=0,temp;
-    scanf("%d %d",&m,&n);
-    int array[m][n];
-    for (int i=0;i<m;i++){
-        for (int j = 0; j < n; j++)
-        {
-            scanf("%d",&array[i][j]);
-        }
-    }
-    for (int i=0;i<m;i++){
-        for (int j = 0; j < n; j++)
-        {
-            temp = array[i][j];
-            printf("%d ",temp);
-            tempsum += temp;
-        }
-        printf("%d",tempsum);
-        printf("\n");
-        endsum += tempsum;
-        tempsum=0;
-    }
+    int n;
+    scanf("%d",&n);
+    int think[n+1];
+    // input array think
     for (int i=0;i<n;i++){
-        for (int j = 0; j < m; j++)
-        {
-            temp = array[j][i];
-            tempsum += temp;
-        }
-        printf("%d ",tempsum);
-        tempsum=0;
+        scanf("%d",&think[i]);
     }
-    printf("%d",endsum);
-
+    // a)
+    printf("a) ");
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%d ",think[i]);
+    }
+    printf("\n");
+    // b
+    printf("b) ");
+    int max = think[2];
+    int indexAmount = 0;
+    for(int i =0;i<n;i++){
+        if (think[i]>max)
+        {
+            indexAmount++;
+        }
+    }
+    int index[indexAmount];
+    int morethanmax[indexAmount];
+    int runner =0;
+    for (int j = 0; j < indexAmount; j++){
+        for (int i = runner;i<n;i++){
+            runner++;
+            if (think[i]>max){
+                index[j]=i;
+                morethanmax[j]=think[i];
+            }
+        }
+    }
+    for (int i = 0; i < indexAmount; i++)
+    {
+        printf("%d ",morethanmax[i]);
+    }
+    
+    printf("\n");
+    // c
+    printf("c) ",index);
+    for (int i = 0; i < indexAmount; i++)
+    {
+        printf("%d ",index[i]);
+    }
+    printf("\n");
+    // d
+    n+=1;
+    think[n-1]= think[2]+think[3];
+    printf("d) ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",think[i]);
+    }
+    printf("\n");
+    // e
+    printf("e) ");
+    think[3]=abs(think[2]-think[4]);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",think[i]);
+    }
+    printf("\n");
+    // f
+    printf("f) ");
+    if (think[1]>think[5])
+    {
+        printf("2");
+    }
+    else{
+        printf("6");
+    }
+    printf("\n");
+    // g
+    printf("g) ");
+    int temp=think[1];
+    think[1]=think[n-1];
+    think[n-1]=temp;
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",think[i]);
+    }
+    printf("\n");
+    // h
+    printf("h) ");
+    int amount =0;
+    for (int i = 0; i < n; i++)
+    {
+        if (think[i]%2 ==0){
+            amount+=1;
+        }
+    }
+    int new[amount];
+    runner =0;
+    for (int i = 0; i < amount; i++)
+    {
+        for (int j = runner; j < n; j++)
+        {
+            runner+=1;
+            if (think[j]%2 == 0)
+            {
+                new[i]=think[j];
+                break;
+            }
+            
+        }
+    }
+    for (int i = 0; i < amount; i++)
+    {
+        printf("%d ",new[i]);
+    }
     return 0;
 }
